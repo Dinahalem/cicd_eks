dc1f295c31bcf5f4f0d91001da09381336c97fae
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-07 08:56:36 +0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,14 +1,15 @@
 provider "aws" {
-  region     = var.aws_region
+  region = var.aws_region
   access_key = "AKIA6GBMEYMUE2PHTPC6"
   secret_key = "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"
 }
 
 data "aws_availability_zones" "available" {}
 
+
 locals {
-  cluster1_name = "eks-cluster1"
-  cluster2_name = "eks-cluster2"
+  cluster1_name = "primary-cluster"
+  cluster2_name = "secondary-cluster"
 }
 
 resource "random_string" "suffix" {
@@ -16,14 +17,14 @@ resource "random_string" "suffix" {
   special = false
 }
 
-module "vpc" {
+module "vpc_cluster1" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
   name                 = "eks-vpc"
-  cidr                 = "10.0.0.0/16"  # Adjust CIDR block as needed
+  cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]  # Adjust subnet configurations as needed
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -32,18 +33,16 @@ module "vpc" {
 
   tags = {
     "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-    "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
   }
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-    "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-    "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
 }
+
