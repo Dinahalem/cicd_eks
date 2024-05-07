a6b23f2fa9ca379efd31ba26b1444905c1ccafd4
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-07 05:48:49 +0300

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,16 +1,14 @@
-
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = "AKIA6GBMEYMUE2PHTPC6"
   secret_key = "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"
 }
 
 data "aws_availability_zones" "available" {}
 
-
 locals {
-  cluster1_name = "primary-cluster"
-  cluster2_name = "secondary-cluster"
+  cluster1_name = "eks-cluster1"
+  cluster2_name = "eks-cluster2"
 }
 
 resource "random_string" "suffix" {
@@ -18,14 +16,14 @@ resource "random_string" "suffix" {
   special = false
 }
 
-module "vpc_cluster1" {
+module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
   name                 = "eks-vpc"
-  cidr                 = var.vpc_cidr
+  cidr                 = "10.0.0.0/16"  # Adjust CIDR block as needed
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]  # Adjust subnet configurations as needed
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -34,48 +32,17 @@ module "vpc_cluster1" {
 
   tags = {
     "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
-}
-
-
-
-
-#cluster2
-
-module "vpc_cluster2" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.7.0"
-
-  name                 = "eks-vpc-cluster2"
-  cidr                 = "10.1.0.0/16"  # Adjust CIDR block as needed
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.1.1.0/24", "10.1.2.0/24"]  
-  public_subnets       = ["10.1.4.0/24", "10.1.5.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  tags = {
     "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
   }
 
   public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
     "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
     "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster1_name}" = "shared"
     "kubernetes.io/cluster/${local.cluster2_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
