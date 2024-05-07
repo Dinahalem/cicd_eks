9d0427756247f7c533c0fe5d16f846439ce05187
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-07 05:57:51 +0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,16 +1,31 @@
-output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
+output "cluster1_id" {
+  description = "EKS cluster ID for cluster 1."
+  value       = module.eks_cluster1.cluster_id
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane."
-  value       = module.eks.cluster_endpoint
+output "cluster1_endpoint" {
+  description = "Endpoint for EKS control plane for cluster 1."
+  value       = module.eks_cluster1.cluster_endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane."
-  value       = module.eks.cluster_security_group_id
+output "cluster1_security_group_id" {
+  description = "Security group ids attached to the control plane for cluster 1."
+  value       = module.eks_cluster1.cluster_security_group_id
+}
+
+output "cluster2_id" {
+  description = "EKS cluster ID for cluster 2."
+  value       = module.eks_cluster2.cluster_id
+}
+
+output "cluster2_endpoint" {
+  description = "Endpoint for EKS control plane for cluster 2."
+  value       = module.eks_cluster2.cluster_endpoint
+}
+
+output "cluster2_security_group_id" {
+  description = "Security group ids attached to the control plane for cluster 2."
+  value       = module.eks_cluster2.cluster_security_group_id
 }
 
 output "region" {
@@ -19,5 +34,5 @@ output "region" {
 }
 
 output "oidc_provider_arn" {
-  value = module.eks.oidc_provider_arn
+  value = module.eks_cluster1.oidc_provider_arn  # Assuming both clusters have the same OIDC provider ARN
 }
