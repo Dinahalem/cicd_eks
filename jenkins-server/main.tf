03bf4e3d755b47351b13ff6947a6453a27731c4b
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-19 09:24:08 +0300

    Update main.tf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -5,15 +5,18 @@ module "vpc" {
   cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.azs.names
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  public_subnets  = var.public_subnets
+  
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = true
 
   tags = {
-    Terraform = "true"
+    Name        = "jenkins_vpc"
+    Terraform   = "true"
     Environment = "dev"
   }
+  public_subnet_tags = {
+    Name = "jenkins_subnet"
+  }
 }
-	
+
