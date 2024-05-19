bec03d043a8b3fdf784ec6a7b1063d8aae838198
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-19 11:17:22 +0300

    Update main.tf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -1,12 +1,13 @@
+#Vpc
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "jenkins_vpc"
   cidr = var.vpc_cidr
 
-  azs             = data.aws_availability_zones.azs.names
-  public_subnets  = var.public_subnets
-  
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets
+
   enable_dns_hostnames    = true
   map_public_ip_on_launch = true
 
@@ -20,3 +21,41 @@ module "vpc" {
   }
 }
 
+#sg 
+
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins_sg"
+  description = "Security group for jenkins server"
+  vpc_id      = module.vpc.vpc_id
+
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  tags = {
+    Name = "jenkins_sg"
+  }
+}
