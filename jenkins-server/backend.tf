903f17f6fd742717bcd7f87a262e8239c136114b
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-19 09:28:12 +0300

    Update backend.tf

--- jenkins-server/backend.tf
+++ jenkins-server/backend.tf
@@ -1,7 +1,7 @@
 terraform {
-    backend "s3" {
-	    bucket = "dina-eks-jenkins"
-		key = "jenkins/terraform.tfstate"
-		region = "us-east-2"
-	}	
-} 
+  backend "s3" {
+    bucket = "dina-eks-jenkins"
+    key    = "jenkins/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
