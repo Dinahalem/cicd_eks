a52b025ad0b3b79e5f63b6ee0fec03ae82fd3e85
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-21 08:44:43 +0300

    Create main.tf

---
+++ EKS/main.tf
@@ -0,0 +1,22 @@
+#Vpc
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eks_cluster_vpc"
+  cidr = var.vpc_cidr
+
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets
+
+  enable_dns_hostnames    = true
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "jenkins_vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
+  public_subnet_tags = {
+    Name = "jenkins_subnet"
+  }
+}
