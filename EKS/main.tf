8f1ce17fe02480ba336cb98594db53c82030c93a
Dina Abd El-Halim <91435311+Dinahalem@users.noreply.github.com>
2024-05-21 09:11:02 +0300

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -5,13 +5,14 @@ module "vpc" {
   name = "eks_cluster_vpc"
   cidr = var.vpc_cidr
 
-  azs            = data.aws_availability_zones.azs.names
-  public_subnets = var.public_subnets
+  azs             = data.aws_availability_zones.azs.names
+  public_subnets  = var.public_subnets
   private_subnets = var.private_subnets
 
-  enable_dns_hostnames    = true
-  enable_dns_gateway      = true
-  map_public_ip_on_launch = true
+
+  enable_dns_hostnames = true
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
 
   tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
